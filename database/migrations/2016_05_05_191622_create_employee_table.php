11c79ade5549e1af12fc1319b2629c1ceef6d0b5
thkone <katz.gabriela@gmail.com>
2016-05-05 16:23:48 -0300

    database structure

---
+++ database/migrations/2016_05_05_191622_create_employee_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->string('name');
+            $table->foreign('company_id')->references('id')->on('company')->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employee');
+    }
+}
