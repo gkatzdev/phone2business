23e482724b64e8b766140bb2a4c100ce4b58e992
thkone <katz.gabriela@gmail.com>
2016-05-06 02:53:35 -0300

    css adjusts

--- resources/views/includes/employee/table/table-employees.blade.php
+++ resources/views/includes/employee/table/table-employees.blade.php
@@ -8,8 +8,8 @@
 <div class="employees-table">
     <table id="employees-table" class="table table-hover">
         <thead>
-        <tr>
-            <th>Nome</th>
+        <tr class="table-title">
+            <th class="Nome">Nome</th>
             <th>Empresa</th>
             <th class="text-center"></th>
         </tr>
@@ -18,14 +18,14 @@
         @if(count($data))
             @foreach($data as $employeeKey => $employee)
                 <tr>
-                    <td>
+                    <td class="Nomes">
                         <a class="label btn btn-next" data-toggle="modal" data-target="#modal-employee-details"
                            data-employee-id="{!! $employee->id !!}" data-name="{!! $employee->name !!}"
                            data-company-id="{!! $employee->company_id !!}">
                             {!! $employee->name !!}
                         </a>
                     </td>
-                    <td>{!! $employee->company_name !!}</td>
+                    <td class="table-title">{!! $employee->company_name !!}</td>
                     <td class="text-center">
                         &nbsp;
                         <a class="btn label btn-prev btn-delete-employee" data-token="{!! csrf_token() !!}" data-employee-id="{!! $employee->id !!}"
@@ -37,7 +37,7 @@
                 </tr>
             @endforeach
         @else
-            <tr>
+            <tr class="Nomes">
                 <td colspan="6" class="text-center">Nenhum colaborador cadastrado</td>
             </tr>
         @endif
